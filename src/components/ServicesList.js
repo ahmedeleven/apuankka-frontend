da1d35b68f42e9fde1602f493cbcc27ab12185d8
Ahmed <ahmed.massod@gmail.com>
2023-09-28 17:06:56 +0300

    list service component with initial data

--- src/components/ServicesList.js
+++ src/components/ServicesList.js
@@ -1,7 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import useTokenValidation from "../hooks/useTokenValidation";
+import axios from "axios";
+import { API_BASE_URL } from "../config/config";
 
 function ServicesList() {
-  return <div></div>;
+  const { isLoggedIn, token } = useTokenValidation();
+
+  const [services, setServices] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getServices = () => {
+    const api = axios.create({
+      baseURL: API_BASE_URL,
+    });
+
+    api.defaults.headers.common["Authorization"] = `Token ${token}`;
+
+    api
+      .get("services/")
+      .then((response) => {
+        setServices(response.data);
+
+        //console.log(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Cannot get services:", error);
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    // Call the getServices function when the component mounts
+    getServices();
+  }, []); // The empty dependency array ensures this effect runs once on mount
+
+  console.log("Services:", services);
+  return (
+    <div>
+      {isLoggedIn ? (
+        // Content for authenticated users
+        isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {services.map((service) => (
+              <li key={service.id}>
+                <h3>{service.title}</h3>
+                <p>{service.description}</p>
+                <p>Date: {service.date}</p>
+                <p>Modified: {service.modified}</p>
+              </li>
+            ))}
+          </ul>
+        )
+      ) : (
+        // Content for non-authenticated users
+        <p>Please log in to access this content.</p>
+      )}
+    </div>
+  );
 }
 
 export default ServicesList;
---
+++ src/hooks/useTokenValidation.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
+function useTokenValidation() {
+  // Define a state variable to store the authentication status
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check the token when the component mounts
+    const token = Cookies.get("token");
+
+    if (token) {
+      // If a token is found, consider the user as logged in
+      setIsLoggedIn(true);
+    } else {
+      // No token found, user is not logged in
+      setIsLoggedIn(false);
+    }
+  }, []); // Run this effect only once when the component mounts
+
+  return { isLoggedIn, token: Cookies.get("token") };
+}
+
+export default useTokenValidation;
