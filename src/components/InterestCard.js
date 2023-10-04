eb7e1ad96b2f1132c64e406636fb0dc856627866
Ahmed <ahmed.massod@gmail.com>
2023-10-04 18:14:59 +0300

    handle toggle chosen

--- src/components/InterestCard.js
+++ src/components/InterestCard.js
@@ -1,12 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import handleToggleChosen from "../utils/handleToggleChosen";
 
 function InterestCard({ interest }) {
   const [isContentVisible, setIsContentVisible] = useState(false);
+  const [isChosen, setIsChosen] = useState(interest.chosen); // Initialize isChosen with the value from interest
 
   const toggleContent = () => {
     setIsContentVisible(!isContentVisible);
   };
+
+  const handleToggle = () => {
+    // Dynamically toggle between true and false based on the current isChosen state
+    //const newIsChosen = !isChosen;
+    console.log("Toggling to:", isChosen);
+
+    // Call handleToggleChosen with the new value
+    handleToggleChosen(interest.service_id, interest.user.id, isChosen)
+      .then((updatedIsChosen) => {
+        console.log("Updated isChosen:", updatedIsChosen);
+        setIsChosen(updatedIsChosen); // Update the state with the new value
+      })
+      .catch((error) => {
+        console.error("Error in handleToggleChosen:", error);
+      });
+  };
+
   return (
     <div>
       <div className="d-flex position-relative">
@@ -23,18 +41,30 @@ function InterestCard({ interest }) {
         </div>
         <div className="ms-2 card card-interest">
           <div className="rounded-start-top-0 p-3 rounded">
-            <div className="d-flex justify-content-between">
-              <h6 className="mb-1">{interest.user.username}</h6>
-              <small className="ms-2" onClick={toggleContent}>
-                {isContentVisible ? (
-                  <i className="bi bi-chevron-up"></i>
-                ) : (
-                  <i className="bi bi-chevron-down"></i>
-                )}
-              </small>
+            <div className="d-flex justify-content-between ">
+              <div className="d-flex align-items-center">
+                <h6 className="mb-1">{interest.user.username}</h6>
+              </div>
+              <div className="d-flex align-items-center">
+                <p className="ml-auto mt-2">Chosen</p>
+                <span className="fs-3 ms-2" onClick={handleToggle}>
+                  {isChosen ? (
+                    <i className="bi bi-toggle-on"></i>
+                  ) : (
+                    <i className="bi bi-toggle-off"></i>
+                  )}
+                </span>
+                <small className="ms-2" onClick={toggleContent}>
+                  {isContentVisible ? (
+                    <i className="bi bi-chevron-up"></i>
+                  ) : (
+                    <i className="bi bi-chevron-down"></i>
+                  )}
+                </small>
+              </div>
             </div>
             <div className={isContentVisible ? "collapse show" : "collapse"}>
-              <p className=" mb-0">
+              <p className="mb-0">
                 <i className="bi bi-chat-left-text-fill"></i>
                 <span className="ms-2">
                   {interest.user.profile?.bio
@@ -43,7 +73,7 @@ function InterestCard({ interest }) {
                 </span>
               </p>
 
-              <p className=" mb-0">
+              <p className="mb-0">
                 <i className="bi bi-file-person-fill"></i>
                 <span className="ms-2">
                   {interest.user.profile?.full_name
@@ -52,7 +82,7 @@ function InterestCard({ interest }) {
                 </span>
               </p>
 
-              <p className=" mb-0">
+              <p className="mb-0">
                 <i className="bi bi-telephone-fill"></i>
                 <span className="ms-2">
                   {interest.user.profile?.phone_number
---
+++ src/utils/handleToggleChosen.js
@@ -0,0 +1,50 @@
+import Cookies from "js-cookie";
+import axios from "axios";
+import { API_BASE_URL } from "../config/config";
+
+async function handleToggleChosen(service_id, user_id, initialIsChosen) {
+  // Rename the argument to initialIsChosen
+  const token = Cookies.get("token");
+
+  try {
+    const api = axios.create({
+      baseURL: API_BASE_URL,
+    });
+
+    api.defaults.headers.common["Authorization"] = `Token ${token}`;
+
+    const checkChosenUrl = `interests/service/${service_id}/user/${user_id}/check/`;
+
+    const response = await api.get(checkChosenUrl);
+    const isChosen = response.data.chosen;
+
+    const updateChosenUrl = `interests/service/${service_id}/user/${user_id}/update/`;
+
+    // Determine the new value for 'chosen'
+    const newChosenValue = !initialIsChosen; // Use initialIsChosen here
+
+    const method = "PUT";
+
+    // Send the updated data with 'chosen' set to the new value
+    const toggleResponse = await api({
+      method,
+      url: updateChosenUrl,
+      data: {
+        chosen: newChosenValue,
+      },
+    });
+
+    if (toggleResponse.status === 200) {
+      return newChosenValue;
+    } else {
+      console.error("Error toggling interest:", toggleResponse.data);
+      return isChosen; // Return the previous value
+    }
+  } catch (error) {
+    // Handle error
+    console.error("Error handling interest:", error);
+    return false; // Return false in case of an error
+  }
+}
+
+export default handleToggleChosen;
